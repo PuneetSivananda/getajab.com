33192d0533f32e2ffb205294a2ea1489fa9591bd
PuneetSivananda <psivananda@chatsoft.com>
2021-05-31 22:44:08 +0530

    feat: linting added

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -9,7 +9,7 @@ export default function Card() {
 			<div class='card_content'>
 				<h2 class='card_title'>Card Grid Layout</h2>
 				<p class='card_text'>Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-				<button class='btn card_btn'>Read More</button>
+				<button class={style.btn}>Read More</button>
 			</div>
 		</div>
 	);
