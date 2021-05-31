e95e343f6326b112d67683b9c38956390306283c
PuneetSivananda <psivananda@chatsoft.com>
2021-05-31 17:19:46 +0530

    feat: updated code

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,16 @@
-import { h, Component } from 'preact';
-import { Link } from 'preact-router';
-import style from './style';
+import { h, Component } from "preact";
+import { Link } from "preact-router";
+import style from "./style";
 
-export default class Header extends Component {
-	render() {
-		return (
-			<header class={style.header}>
-				<h1>Preact PWA Boilerplate</h1>
-				<nav>
-					<Link href="/">Home</Link>
-					<Link href="/profile">Me</Link>
-					<Link href="/profile/john">John</Link>
-				</nav>
-			</header>
-		);
-	}
-}
+const Header = () => {
+	return (
+		<header class={style.header}>
+			<h1>Preact PWA Boilerplate Test</h1>
+			<nav>
+				<Link href="/">Home</Link>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
