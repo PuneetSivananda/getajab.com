3e3862925ec8a0994f307d64a41957071958a40d
PuneetSivananda <psivananda@chatsoft.com>
2021-05-31 23:51:45 +0530

    feat: updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,16 @@ const path = require('path')
 const prettierOptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'))
 
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  plugins: ['prettier', 'react', 'react-hooks', 'jsx-a11y'],
-  env: {
-    jest: true,
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    'no-shadow': 'off',
     'prettier/prettier': ['error', prettierOptions],
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
 }
