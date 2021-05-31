c3532f5a51a167ecf2da7edce860cb4de6f5ff90
PuneetSivananda <psivananda@chatsoft.com>
2021-05-31 21:16:42 +0530

    wip: cards

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,13 +1,19 @@
 import { FunctionalComponent, h } from 'preact';
 import style from './style.css';
-
+import Card from '../../components/card/index';
+// ["🚀", "😺", "🐶", "🏈", "📦", "🙊"]
 const Home: FunctionalComponent = () => {
-    return (
-        <div class={style.home}>
-            <h1>Home</h1>
-            <p>This is the Home component.</p>
-        </div>
-    );
+	return (
+		<div class={style.home}>
+			<h1>Home</h1>
+			<p>This is the Home component.</p>
+			<div class={style.grid}>
+				<div class={style.game}>
+					<Card hiddenValue={'😺'} flipStatus={'DEFAULT'} onClick={() => alert('Check')} />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
