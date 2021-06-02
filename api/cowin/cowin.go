174c5883f50b14437e3b8fc65b941d7d2cc99cb9
PuneetSivananda <psivananda@chatsoft.com>
2021-06-02 21:01:36 +0530

    feat: updates to response types

---
+++ api/cowin/cowin.go
@@ -0,0 +1,29 @@
+package cowin
+
+type Response struct {
+	Centers []Center `json:"centers"`
+}
+
+type Center struct {
+	CenterId int       `json:"center_id"`
+	Name     string    `json:"name"`
+	Addr     string    `json:"address"`
+	State    string    `json:"state_name"`
+	District string    `json:"district_name"`
+	Block    string    `json:"block_name"`
+	Pin      string    `json:"pincode"`
+	Lat      int       `json:"lat"`
+	Long     int       `json:"long"`
+	Fee      string    `json:"fee_type"`
+	Sessions []Session `json:"sessions"`
+}
+
+type Session struct {
+	SessionID         string `json:"session_id"`
+	Date              string `json:"date"`
+	AvailableCapacity int    `json:"available_capacity"`
+	MinAge            int    `json:"min_age_limit"`
+	Vaccine           string `json:"vaccine"`
+	Dose1             int    `json:"available_capacity_dose1"`
+	Dose2             int    `json:"available_capacity_dose2"`
+}
