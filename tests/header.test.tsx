5820b60961ad146f1be992850d406835c5820357
PuneetSivananda <psivananda@chatsoft.com>
2021-06-02 13:36:31 +0530

    fix: testing husky

--- tests/header.test.tsx
+++ tests/header.test.tsx
@@ -1,20 +1,19 @@
-/** @jsx h */
-import { h } from 'preact'
-import Header from '../src/components/header'
+import { h } from 'preact';
+import Header from '../src/components/header';
 // See: https://github.com/preactjs/enzyme-adapter-preact-pure
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
 
 describe('Initial Test of the Header', () => {
-  test('Header renders 3 nav items', () => {
-    const context = shallow(<Header />)
-    expect(context.find('h1').text()).toBe('Covid Portal - Bengaluru')
-    expect(context.find('Link').length).toBe(1)
-  })
-})
+	test('Header renders 3 nav items', () => {
+		const context = shallow(<Header />);
+		expect(context.find('h1').text()).toBe('Covid Portal - Bengaluru');
+		expect(context.find('Link').length).toBe(1);
+	});
+});
 
 describe('Second Test of the Header', () => {
-  test('Header Displays Peact app', () => {
-    const context = shallow(<Header />)
-    expect(context.find('h1').text()).toBe('Covid Portal - Bengaluru')
-  })
-})
+	test('Header Displays Peact app', () => {
+		const context = shallow(<Header />);
+		expect(context.find('h1').text()).toBe('Covid Portal - Bengaluru');
+	});
+});
