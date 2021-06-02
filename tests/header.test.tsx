17ceaa9e547c1a2aa1ffdf925e8c5bdd668a748d
PuneetSivananda <psivananda@chatsoft.com>
2021-06-02 13:25:34 +0530

    feat: updated tests and card scss

--- tests/header.test.tsx
+++ tests/header.test.tsx
@@ -7,14 +7,14 @@ import { shallow } from 'enzyme';
 describe('Initial Test of the Header', () => {
 	test('Header renders 3 nav items', () => {
 		const context = shallow(<Header />);
-		expect(context.find('h1').text()).toBe('Preact App');
-		expect(context.find('Link').length).toBe(2);
+		expect(context.find('h1').text()).toBe('Covid Portal - Bengaluru');
+		expect(context.find('Link').length).toBe(1);
 	});
 });
 
 describe('Second Test of the Header', () => {
 	test('Header Displays Peact app', () => {
 		const context = shallow(<Header />);
-		expect(context.find('h1').text()).toBe('Preact App');
+		expect(context.find('h1').text()).toBe('Covid Portal - Bengaluru');
 	});
 });
