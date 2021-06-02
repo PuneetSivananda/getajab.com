6957f7520540e29848af285ce32facc3bf09bafd
PuneetSivananda <psivananda@chatsoft.com>
2021-06-02 20:01:54 +0530

    feat:updated basic http server

--- api/main.go
+++ api/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"Hello World")
+}
+
+func setupRoot(){
+	http.HandleFunc("/",homePage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
 
 func main(){
 	fmt.Println("DataApi for the Cowin Stats")
+	setupRoot()
 }
