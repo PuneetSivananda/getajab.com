673f44170cb353cd69fc19930834086dd05918b4
PuneetSivananda <psivananda@chatsoft.com>
2021-06-02 13:35:11 +0530

    fix: testing code and lint

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,27 +1,28 @@
-/* eslint-disable */
-import { FunctionalComponent, h } from 'preact';
-import style from './style.css';
+import { FunctionalComponent, h } from 'preact'
+import style from './style.css'
 
 const Card: FunctionalComponent = () => {
-	return (
-		<div class={style.cardgridspace}>
-			<div class={style.num}>01</div>
-			<a
-				class={style.card}
-				href='https://codetheweb.blog/2017/10/06/html-syntax/'
-				style='--bg-img: url(https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=1500&url=https://codetheweb.blog/assets/img/posts/html-syntax/cover.jpg)'
-			>
-				<div>
-					<h1>HTML Syntax</h1>
-					<p>The syntax of a language is how it works. How to actually write it. Learn HTML syntax…</p>
-					<div class={style.date}>6 Oct 2017</div>
-					<div class={style.tags}>
-						<div class={style.tag}>HTML</div>
-					</div>
-				</div>
-			</a>
-		</div>
-	);
-};
+  return (
+    <div class={style.cardgridspace}>
+      <div class={style.num}>01</div>
+      <a
+        class={style.card}
+        href="https://codetheweb.blog/2017/10/06/html-syntax/"
+        style="--bg-img: url(https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=1500&url=https://codetheweb.blog/assets/img/posts/html-syntax/cover.jpg)"
+      >
+        <div>
+          <h1>HTML Syntax</h1>
+          <p>
+            The syntax of a language is how it works. How to actually write it. Learn HTML syntax…
+          </p>
+          <div class={style.date}>6 Oct 2017</div>
+          <div class={style.tags}>
+            <div class={style.tag}>HTML</div>
+          </div>
+        </div>
+      </a>
+    </div>
+  )
+}
 
-export default Card;
+export default Card
