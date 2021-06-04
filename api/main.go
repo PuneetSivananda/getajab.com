e1777a8113ee361ae5423363b8510267682151e1
PuneetSivananda <psivananda@chatsoft.com>
2021-06-04 15:14:18 +0530

    wip: updates to the status api endpoint

--- api/main.go
+++ api/main.go
@@ -5,8 +5,17 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/PuneetSivananda/cowin-api/cowin"
+	"github.com/PuneetSivananda/cowin-api/websocket"
 )
+
+func CowinData(w http.ResponseWriter, r *http.Request){
+	ws, err:= websocket.Upgrade(w,r)
+	if err!= nil{
+		fmt.Fprintf(w, err)
+	}
+
+
+}
 
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w,"Hello World")
@@ -14,17 +23,12 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func setupRoot(){
 	http.HandleFunc("/",homePage)
+	http.HandleFunc("/data", CowinData)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func main(){
 	fmt.Println("DataApi for the Cowin Stats")
-	
-	data, err:= cowin.GetWeeklyData()
-	if err!=nil{
-		fmt.Println(err)
-	}
-	fmt.Printf("%+v\n", data)
-	
-	// setupRoot()
+
+	setupRoot()
 }
