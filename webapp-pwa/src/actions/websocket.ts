ad95d8e560e798b3d1311f6c852a2bfe9f4a3925
PuneetSivananda <psivananda@chatsoft.com>
2021-06-07 12:46:31 +0530

    feat: testing lint

--- webapp-pwa/src/actions/websocket.ts
+++ webapp-pwa/src/actions/websocket.ts
@@ -1,35 +1,31 @@
+export const setupWebsocket = ({ host, port }: any) =>
+  new Promise(resolve => {
+    const webSocket = new WebSocket(`ws://${host}:${port}`)
 
-export const setupWebsocket = ({ host, port }:any) =>
-  new Promise((resolve) => {
-    const webSocket = new WebSocket(`ws://${host}:${port}`);
+    const receive = (onMessageCb: any) => {
+      webSocket.onmessage = event => onMessageCb(JSON.parse(event.data))
+    }
 
-    const receive = (onMessageCb:any) => {
-      webSocket.onmessage = (event) => onMessageCb(JSON.parse(event.data));
-    };
+    const send = (type: any, payload: any) => webSocket.send(JSON.stringify({ type, payload }))
 
-    const send = (type:any, payload:any) =>
-      webSocket.send(JSON.stringify({ type, payload }));
-
-    webSocket.onopen = () => resolve({ send, receive });
-  });
-
+    webSocket.onopen = () => resolve({ send, receive })
+  })
 
 export const fetchData = () => {
-    let data = { Data: [] };
-    const websocket = new WebSocket('ws://evening-crag-51333.herokuapp.com/data');
-  
-    websocket.onopen = function(evt) {
-      console.log('Successfully connected to the websocket');
-    };
-  
-    websocket.onerror = function(err) {
-      console.log(err);
-    };
-  
-    websocket.onmessage = function(evnt) {
-      data = JSON.parse(evnt.data);
-      console.log('Sending data to the connected websocket');
-      console.log(data);
-    };
-    return data;
-  };
+  let data = { Data: [] }
+  const websocket = new WebSocket('ws://evening-crag-51333.herokuapp.com/data')
+  websocket.onopen = function(evt) {
+    console.log('Successfully connected to the websocket')
+  }
+
+  websocket.onerror = function(err) {
+    console.log(err)
+  }
+
+  websocket.onmessage = function(evnt) {
+    data = JSON.parse(evnt.data)
+    console.log('Sending data to the connected websocket')
+    console.log(data)
+  }
+  return data
+}
