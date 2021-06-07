36f359f0aea1c98388e704fec0e2790cfff4705b
PuneetSivananda <psivananda@chatsoft.com>
2021-06-07 13:09:59 +0530

    feat: test

--- webapp-pwa/src/components/card/index.tsx
+++ webapp-pwa/src/components/card/index.tsx
@@ -3,21 +3,24 @@ import style from './style.css';
 
 const Card: FunctionalComponent = () => {
 	return (
-		<div class={style.cardgridspace}>
-			<div class={style.num}>01</div>
-			<a
-				class={style.card}
-				style='--bg-img: url(https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=1500&url=https://codetheweb.blog/assets/img/posts/html-syntax/cover.jpg)'
-			>
-				<div>
-					<h1>HTML Syntax</h1>
-					<p>Learn HTML syntax…</p>
-					<div class={style.date}>6 Oct 2017</div>
-					<div class={style.tags}>
-						<div class={style.tag}>HTML</div>
+		<div>
+			<div />
+			<div class={style.cardgridspace}>
+				<div class={style.num}>01</div>
+				<a
+					class={style.card}
+					style='--bg-img: url(https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=1500&url=https://codetheweb.blog/assets/img/posts/html-syntax/cover.jpg)'
+				>
+					<div>
+						<h1>HTML Syntax</h1>
+						<p>Learn HTML syntax…</p>
+						<div class={style.date}>6 Oct 2017</div>
+						<div class={style.tags}>
+							<div class={style.tag}>HTML</div>
+						</div>
 					</div>
-				</div>
-			</a>
+				</a>
+			</div>
 		</div>
 	);
 };
