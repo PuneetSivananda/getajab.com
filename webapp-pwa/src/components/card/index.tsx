50a0e91a0c62398b2c72aa1d51b9cf2471cbdbe4
PuneetSivananda <psivananda@chatsoft.com>
2021-06-07 14:58:55 +0530

    feat: added a colored line to indicate status

--- webapp-pwa/src/components/card/index.tsx
+++ webapp-pwa/src/components/card/index.tsx
@@ -1,24 +1,37 @@
 import { FunctionalComponent, h } from 'preact';
 import style from './style.css';
 interface Props {
-  image: string;
-  title: string;
-  body: string;
+	image: string;
+	title: string;
+	body: string;
 }
+const ColoredLine = ({ color }) => (
+	<hr
+		style={{
+			color: color,
+			backgroundColor: color,
+			height: 5,
+			width: '100%',
+			margin: 0
+		}}
+	/>
+);
+
 const Card: FunctionalComponent = ({ image, title, body }: Props) => {
-  return (
-    <article class={style.card}>
-      <a href="#">
-        <figure class="thumbnail">
-          <img src={image} alt="meow" />
-        </figure>
-        <div class={style.cardContent}>
-          <h2>{title}</h2>
-          <p>{body}</p>
-        </div>
-      </a>
-    </article>
-  );
+	return (
+		<article class={style.card}>
+			<a href='#'>
+				<ColoredLine color='red' />
+				<figure class='thumbnail'>
+					<img src={image} alt='meow' />
+				</figure>
+				<div class={style.cardContent}>
+					<h2>{title}</h2>
+					<p>{body}</p>
+				</div>
+			</a>
+		</article>
+	);
 };
 
 export default Card;
