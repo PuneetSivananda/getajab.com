d246cac9d31868ffbc43f9c541e54853b3680582
PuneetSivananda <psivananda@chatsoft.com>
2021-06-07 13:16:40 +0530

    feat: test

--- webapp-pwa/src/components/card/index.tsx
+++ webapp-pwa/src/components/card/index.tsx
@@ -4,7 +4,6 @@ import style from './style.css'
 const Card: FunctionalComponent = () => {
   return (
     <div>
-      <div />
       <div class={style.cardgridspace}>
         <div class={style.num}>01</div>
         <a
