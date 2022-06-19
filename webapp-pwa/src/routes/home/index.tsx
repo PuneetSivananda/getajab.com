45c7f59d03a4e05e3eaf34658ffd64ffb48dba01
PuneetSivananda <puneet.sivananda@gmail.com>
2022-06-19 22:37:02 +0530

    wip: card styles imported and updated

--- webapp-pwa/src/routes/home/index.tsx
+++ webapp-pwa/src/routes/home/index.tsx
@@ -4,7 +4,8 @@ import style from "./style.css";
 import cardStyle from "./card.css"
 import CardSection from "../../components/CardSection";
 import bgImg from "../../assets/img/image-equilibrium.jpg"
-
+import iconEth from "../../assets/img/icon-ethereum.svg"
+import clockIcon from "../../assets/img/icon-clock.svg"
 const Home: FunctionalComponent = () => {
 	const geolocation: any[] = [];
 	let lat;
@@ -30,22 +31,20 @@ const Home: FunctionalComponent = () => {
 				</div>
 				<h5 class={cardStyle.cardTitle}>Runing #3445</h5>
 				<p class={cardStyle.cardDescription}>
-					As Advertising company we are earger to serve you in promoting your product
-					<a href="#">FEISAL ISSACK</a> 'CEO OF THE COMPANY'
+					As Advertising company we are earger to serve you in promoting your product.
 				</p>
 
-				<p class="crypto-price">
-					<img src="./img/icon-ethereum.svg" class="crypto-symbol" /> 0.051
+				<p class={cardStyle.cryptoPrice}>
+					<img src={iconEth} class={cardStyle.cryptoSymbol} /> 0.051
 					ETH
 				</p>
-				<p class="days-indicator">
-					<img src="./img/icon-clock.svg" class="clock-img" /> 4 days left
+				<p class={cardStyle.daysIndicator}>
+					<img src={clockIcon} class={cardStyle.clockImg} /> 4 days left
 				</p>
-				<hr class="divider" />
+				<hr class={cardStyle.divider} />
 
-				<p class="creator">
-					<img src="./img/ceo img.png" class="avatar" />
-					Creation of <span class="creator-name">Feisal Issack</span>
+				<p class={cardStyle.creator}>
+					Creation of <span class={cardStyle.creatorName}>Feisal Issack</span>
 				</p>
 			</div>
 		</main>
