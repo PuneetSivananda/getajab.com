97eb5f2b22f7d45cb0e117793e0675f8408f1cc4
PuneetSivananda <puneet.sivananda@gmail.com>
2022-12-29 00:06:27 +0530

    wip: layout settings

---
+++ findmyjab-ui/src/routes/Covid/+page.svelte
@@ -0,0 +1,7 @@
+<main>
+    <div class="relative pt-10 pb-20 sm:py-24">
+        <div class="absolute inset-x-0 -top-48 -bottom-14 overflow-hidden bg-indigo-50">
+            Hello World
+        </div>
+    </div>  
+</main>
